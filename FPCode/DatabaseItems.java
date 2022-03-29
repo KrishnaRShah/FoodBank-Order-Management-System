92ecb964d794ea1238c6687af98f5380360f7bc6
LowkeyMadLad <ryan.mailhiot@ucalgary.ca>
2022-03-28 21:19:10 -0600

    These are the current updates for DatabaseItems and Items. There will be future changes coming once the rest of the files needed have been created

--- FPCode/DatabaseItems.java
+++ FPCode/DatabaseItems.java
@@ -1,5 +1,81 @@
 package FPCode;
 
+import java.sql.*;
+import java.util.Vector;
+
 public class DatabaseItems {
-    
+    private final int SIZE_VECTOR = 20;
+    private final int INCREMENT_VECTOR = 10;
+    public final String DBURL = "jdbc:mysql://localhost/food_inventory";
+    public final String USERNAME = "student";
+    public final String PASSWORD = "ensf"; 
+    private static Vector<Items> databaseItems;
+    private Connection dbConnect;
+
+
+    protected DatabaseItems(){
+        databaseItems = new Vector<Items>(SIZE_VECTOR, INCREMENT_VECTOR);
+        refreshDatabaseItems();
+    }
+    public void initializeConnection(){
+        try {
+            dbConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace(); // THIS NEEDS TO BE CHANGED BECAUSE WE NEED TO BE ABLE TO INTERNALLY HANDLE ERRORS
+        }
+    }
+
+    public void refreshDatabaseItems(){
+        // Call to database to return all from the given inventory
+        databaseItems.clear();
+        Items itemAdd;
+        Double[] nutrientsInfo = new Double[5];
+        try {
+            String query = "SELECT * FROM available_food";
+            PreparedStatement myStmt = dbConnect.prepareStatement(query);
+            ResultSet results = myStmt.executeQuery();
+
+            while (results.next()) {
+                nutrientsInfo[0] = (double)results.getInt("GrainContent");
+                nutrientsInfo[1] = (double)results.getInt("FVContent");
+                nutrientsInfo[2] = (double)results.getInt("ProContent");
+                nutrientsInfo[3] = (double)results.getInt("Other");
+                nutrientsInfo[4] = (double)results.getInt("Calories");
+                itemAdd = new Items(results.getInt("ItemID"), results.getString("Name"), nutrientsInfo);
+                databaseItems.add(itemAdd);
+            }
+            myStmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void updateDatabase(Items[] items){
+        // List of items to remove from data base.
+        try {
+            String query = "DELETE FROM available_food WHERE ItemID = ?";
+            PreparedStatement myStmt = dbConnect.prepareStatement(query);
+            for (int i = 0; i < items.length; i++) {
+                myStmt.setInt(1, items[i].getITEM_ID());
+
+                int updateCheck = myStmt.executeUpdate();
+                if (updateCheck != 1) {
+                    throw new SQLException();
+                } else {
+                    updateCheck = 0;
+                }
+                myStmt.clearParameters();
+            }
+            myStmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        refreshDatabaseItems();
+    }
+
+    public Items getLargestItem(String type) { // THIS WILL BE COMPLETED ONCE NUTRIENT CLASS IS MADE
+        return null;
+    }
+
 }
--- FPCode/Items.java
+++ FPCode/Items.java
@@ -1,5 +1,34 @@
 package FPCode;
 
 public class Items {
-    
+    private Nutrients nutrientData;
+    private final int ITEM_ID;
+    private final String ITEM_NAME;
+
+    public Items (int id, String name, Double[] nutrientsInfo){
+        this.nutrientData = new Nutrients(nutrientsInfo[0], nutrientsInfo[1], nutrientsInfo[2], nutrientsInfo[3], nutrientsInfo[4]);
+        this.ITEM_NAME = name;
+        this.ITEM_ID = id;
+    }
+
+
+    public Nutrients getNutrientData() {
+        return this.nutrientData;
+    }
+
+    public void setNutrientData(Nutrients nutrientData) {
+        this.nutrientData = nutrientData;
+    }
+
+    public int getITEM_ID() {
+        return this.ITEM_ID;
+    }
+
+
+    public String getITEM_NAME() {
+        return this.ITEM_NAME;
+    }
+
+
+
 }
