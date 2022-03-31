9cccaf43af993fe1486fca4c43e34fdd359bbbd5
KrishnaRShah <krishnashah14957@gmail.com>
2022-03-30 22:07:32 -0600

    mini order changes

--- FPCode/Order.java
+++ FPCode/Order.java
@@ -1,5 +1,25 @@
 package FPCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    public List<Hamper> hamperList = new ArrayList<>();
+    public Order(){
+        
+
+
+    }
+
+    public Hamper getHampterAtIndex(int index){
+        return this.hamperList.get(index);
+    }
+
+    public List<Hamper> getHamper(){
+        return this.hamperList;
+    }
+    
+    
     
 }
---
+++ FPCode/OrderTest.java
@@ -0,0 +1,17 @@
+
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class OrderTest {
+
+    @Test
+    public void testOrderConstructor(){
+
+    }
+    @Test
+    public void testOrderConstructor(){
+        
+    }
+    
+}
