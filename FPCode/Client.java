51dc0e74b0a2fd6435cd8d46a5c141e58eef261d
LowkeyMadLad <ryan.mailhiot@ucalgary.ca>
2022-04-09 21:53:09 -0600

    Updates to Client and ClientType to add the connection function.
    Patch Notes:
    ClientType:
    - Added typeAsString method which returns the given type as a string. These phrases can be changed to suit our needs.
    - Javadoc comments were added to reflect the state of the file.
    
    Client:
    - Added a new Client constructor to only take in client ID instead of type as well. This constructor will set everything to do with the client information
    - Added Database connectivity
    - Commented out several items (review commented out parts for more details)
    - Added Javadoc comments to all methods.
    - Updated version number and all *known* authors of the file. In order of work done.

--- FPCode/Client.java
+++ FPCode/Client.java
@@ -1,64 +1,204 @@
+/**
+* @author German (David) Fonseca 30061209<a
+* href="mailto:german.fonseca@ucalgary.ca">german.fonseca@ucalgary.ca</a>
+* @author Ryan Mailhiot 30080009<a
+* href="mailto:ryan.mailhiot@ucalgary.ca">ryan.mailhiot@ucalgary.ca</a>
+* @author Danny Picazo 301271082<a
+* href="mailto:daniel.picazo@ucalgary">daniel.picazo@ucalgary.ca</a>
+* @version 0.8 
+* @since 0.0
+*/
+
 package FPCode;
 
+import java.sql.*;
+
 public class Client {
+
+    // enum ClientType {
+    //     ADULTMALE{
+    //         public String typeAsString(){
+    //             return "Adult Male";
+    //         }
+    //     },
+    //     ADULTFEMALE{
+    //         public String typeAsString(){
+    //             return "Adult Female";
+    //         }
+    //     },
+    //     CHILDOVER8{
+    //         public String typeAsString(){
+    //             return "Child Over 8 years old";
+    //         }
+    //     },
+    //     CHILDUNDER8{
+    //         public String typeAsString(){
+    //             return "Child Under 8 years old";
+    //         }
+    //     };
+    //     public abstract String typeAsString();
+    // }
+
     private Nutrients nutrientData;
     private final int CLIENT_ID;
     private final String CLIENT_TYPE;
+    private final String DBURL = "jdbc:mysql://localhost/food_inventory";
+    private final String USERNAME = "student";
+    private final String PASSWORD = "ensf"; 
+    private Connection dbConnect;
 
-    // temporary hardcoded client type values until we figured out
-    // whatever the hell it's supposed to be
-    Client(int id, String type){
-        // 4 possible IDs
+    /**
+     * Constructor for the object Client. Uses the enum "ClientType" for the client type registration. Takes in an id value for
+     * the type of client and sets nutrientData, CLIENT_ID and CLIENT_TYPE;
+     * @author Ryan Mailhiot
+     * @param id takes a value from 1-4 (inclusive) for the type of client. 1 is Adult Male, 2 is Adult Female, 3 is Child
+     * Over 8 years old, 4 is Child under 8 years old.
+     */
+    public Client(int id){
         if(id < 1 || id > 4){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("Client ID is invalid.");
         }
-        // set nutrients based on the type
-        String temp = type.toUpperCase();
-        switch (temp) {
-            case "AM":
-                nutrientData = new Nutrients(16, 28, 26, 30, 2500);
-                type = "Adult Male";
-                // If AM is always 1, then why do we even need to send it
-                // as an argument? Can't we just send the client ID to the
-                // constructor and have this switch-case set types? - Danny
-                if(id != 1) throw new IllegalArgumentException();
+        // Initializes the connection with the database and closes later.
+        initializeConnection();
+        // This switch identifies the client.
+        switch (id) {
+            case 1:
+                this.CLIENT_ID = id;
+                this.CLIENT_TYPE = ClientType.ADULTMALE.typeAsString();
                 break;
-            case "AF":
-                nutrientData = new Nutrients(16, 28, 26, 30, 2000);
-                type = "Adult Female";
-                if(id != 2) throw new IllegalArgumentException();
+
+            case 2:
+                this.CLIENT_ID = id;
+                this.CLIENT_TYPE = ClientType.ADULTFEMALE.typeAsString();
                 break;
-            case "CO8":
-                nutrientData = new Nutrients(21, 33, 31, 15, 2200);
-                type = "Child Over 8";
-                if(id != 3) throw new IllegalArgumentException();
+
+            case 3:
+                this.CLIENT_ID = id;
+                this.CLIENT_TYPE = ClientType.CHILDOVER8.typeAsString();
                 break;
-            case "CU8":
-                nutrientData = new Nutrients(21, 33, 31, 15, 1400);
-                type = "Child Under 8";
-                if(id != 4) throw new IllegalArgumentException();
+
+            case 4:
+                this.CLIENT_ID = id;
+                this.CLIENT_TYPE = ClientType.CHILDUNDER8.typeAsString();
                 break;
+        
             default:
-                type = "";
-                throw new IllegalArgumentException();
+                this.CLIENT_ID = 0;
+                this.CLIENT_TYPE = "Default Case";
+                break;
         }
-        // no error, so commence forth
-        this.CLIENT_ID = id;
-        this.CLIENT_TYPE = type;
+
+        try {
+            String query = "SELECT FROM daily_client_needs WHERE ClientID = ?";
+            PreparedStatement myStmt = dbConnect.prepareStatement(query);
+            myStmt.setInt(1, id);
+            ResultSet results = myStmt.executeQuery();
+            
+            if (results.next()) {
+                this.nutrientData = new Nutrients((double)results.getInt("WholeGrains"),
+                (double)results.getInt("FruitVeggies"), (double)results.getInt("Protein"),
+                (double)results.getInt("Other"), (double)results.getInt("Calories"));
+            }
+            myStmt.close();
+            results.close();
+            close();
+        } catch (Exception e) {
+            e.printStackTrace(); // THIS NEEDS TO BE CHANGED BECAUSE WE NEED TO BE ABLE TO INTERNALLY HANDLE ERRORS
+        }
     }
 
+
+
+    // Client(int id, String type){
+    //     // 4 possible IDs
+    //     if(id < 1 || id > 4){
+    //         throw new IllegalArgumentException();
+    //     }
+    //     // set nutrients based on the type
+    //     String temp = type.toUpperCase();
+    //     switch (temp) {
+    //         case "AM":
+    //             nutrientData = new Nutrients(16, 28, 26, 30, 2500);
+    //             type = "Adult Male";
+    //             // If AM is always 1, then why do we even need to send it
+    //             // as an argument? Can't we just send the client ID to the
+    //             // constructor and have this switch-case set types? - Danny
+
+                
+    //             if(id != 1) throw new IllegalArgumentException();
+    //             break;
+    //         case "AF":
+    //             nutrientData = new Nutrients(16, 28, 26, 30, 2000);
+    //             type = "Adult Female";
+    //             if(id != 2) throw new IllegalArgumentException();
+    //             break;
+    //         case "CO8":
+    //             nutrientData = new Nutrients(21, 33, 31, 15, 2200);
+    //             type = "Child Over 8";
+    //             if(id != 3) throw new IllegalArgumentException();
+    //             break;
+    //         case "CU8":
+    //             nutrientData = new Nutrients(21, 33, 31, 15, 1400);
+    //             type = "Child Under 8";
+    //             if(id != 4) throw new IllegalArgumentException();
+    //             break;
+    //         default:
+    //             type = "";
+    //             throw new IllegalArgumentException();
+    //     }
+    //     // no error, so commence forth
+    //     this.CLIENT_ID = id;
+    //     this.CLIENT_TYPE = type;
+    // }
+    
+    /**
+     * Creates an instance called dbConnect to connect to the database and grab the Daily Client Needs. 
+     */
+    public void initializeConnection(){
+        try {
+            dbConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace(); // THIS NEEDS TO BE CHANGED BECAUSE WE NEED TO BE ABLE TO INTERNALLY HANDLE ERRORS
+        }
+    }
+
+    /**
+     * Closes the dbConnect variable to stop unnecessary leak.
+     */
+    public void close() {
+        try {
+            dbConnect.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }          
+
+    }
+
+    /**
+     * Getter for the nutrientData of the client.
+     * @return Nutrients Object
+     */
     public Nutrients getNutrientData() {
         return this.nutrientData;
     }
 
-    public void setNutrientData(Nutrients nutrientData) {
-        this.nutrientData = nutrientData;
-    }
-
+    // COMMENTED OUT BECAUSE IT SEEMS EXTREMELY UNNECESSARY - Ryan
+    // public void setNutrientData(Nutrients nutrientData) {
+    //     this.nutrientData = nutrientData;
+    // }
+    
+    /**
+     * Getter for the Client ID
+     * @return Int
+     */
     public int getClientID() {
         return this.CLIENT_ID;
     }
 
+    /**
+     * Getter for the ClientType
+     * @return String
+     */
     public String getClientType() {
         return this.CLIENT_TYPE;
     }
--- FPCode/ClientType.java
+++ FPCode/ClientType.java
@@ -1,9 +1,41 @@
+/**
+* @author Ryan Mailhiot 30080009<a
+* href="mailto:ryan.mailhiot@ucalgary.ca">ryan.mailhiot@ucalgary.ca</a>
+* @version 0.2 
+* @since 0.0
+*/
+
 package FPCode;
 
+/**
+ * ClientType enum is used in client to set the type of client. Contains ADULTMALE, ADULTFEMALE, CHILDOVER8, CHILDUNDER8
+ */
 public enum ClientType {
-    ADULTMALE,
-    ADULTFEMALE,
-    CHILDOVER8,
-    CHILDUNDER8;
+    ADULTMALE{
+        public String typeAsString(){
+            return "Adult Male";
+        }
+    },
+    ADULTFEMALE{
+        public String typeAsString(){
+            return "Adult Female";
+        }
+    },
+    CHILDOVER8{
+        public String typeAsString(){
+            return "Child Over 8 years old";
+        }
+    },
+    CHILDUNDER8{
+        public String typeAsString(){
+            return "Child Under 8 years old";
+        }
+    };
+    /**
+     * Returns a string of the type of client.
+     * @return String
+     */
+    public abstract String typeAsString();
 }
+
 
