edf6ae9504f7eb8c06b1b1185bb867f189e628da
LowkeyMadLad <ryan.mailhiot@ucalgary.ca>
2022-04-09 22:13:21 -0600

    Update to Hamper
    Patch Notes:
    - Added Javadoc comments to almost all methods and constructors
    - Updated the client constructor to use the new method of only ID

--- FPCode/Hamper.java
+++ FPCode/Hamper.java
@@ -1,19 +1,47 @@
+/**
+ * @author Danny Picazo 301271082<a
+ * href="mailto:daniel.picazo@ucalgary">daniel.picazo@ucalgary.ca</a>
+ * @author German (David) Fonseca 30061209<a
+ * href="mailto:german.fonseca@ucalgary.ca">german.fonseca@ucalgary.ca</a>
+ * @version 0.6 
+ * @since 0.0
+ */
+
+
 package FPCode;
 import java.util.*;
 
+/**
+ * Class Hamper is a custom object class that contains information about clients, nutritional data for people using the hamper
+ * and the days needed for the hamper. This acts as the "connecting" point between all other connections in the program.
+ * Hamper interacts with almost all other files in some way and is key to understanding the program as a whole. 
+ * 
+ * Hamper will build the most efficient "Hamper" of food for the given clients for the days needed. 
+ * 
+ * UPDATE THE REST OF THIS TO INCLUDE RELEVANT COMMENTS ABOUT HAMPER.
+ */
 public class Hamper{
     public Vector<Client> clientArray;
     public Vector<Items> itemsList;
     public Nutrients hamperNutrients;
     public int daysNeeded;
 
+    /**
+     * Constructor for Hamper. Takes in an integer array of size 4 (or atleast reads the first 4 elements) which will be used
+     * to construct clients to be used in the calculation of hamper nutrients. 
+     * @param numClientTypes Int array of size 4.
+     */
     public Hamper(int[] numClientTypes){
         
         for(int i = 0; i < numClientTypes.length; i++){
-            this.clientArray.addElement(new Client(numClientTypes[i], "AM")); //Hard-Coded an client type for now
+            this.clientArray.addElement(new Client(numClientTypes[i])); //Hard-Coded an client type for now
         }
     }
 
+    /**
+     * @author David Fonseca
+     * Calculates the hampers nutrients using the data stored in the clientArray.
+     */
     public void calcHamperNutrients(){
 
         // Creating variables to hold total macro values, which we will average
@@ -36,20 +64,43 @@ public class Hamper{
 
     }
 
+    /**
+     * INCOMPLETE
+     * @throws NotEnoughFoodException
+     */
     public void buildItemList() throws NotEnoughFoodException{
         
 
     }
 
+    /**
+     * Getter for the clientArray
+     * @return Vector<client>
+     */
     public Vector<Client> getClientArray(){
         return this.clientArray;
     }
+
+    /**
+     * Getter for the getItemsList
+     * @return Vector<Items> (Items object)
+     */
     public Vector<Items> getItemsList(){
         return this.itemsList;
     }
+
+    /**
+     * Getter for the getHamperNutrients
+     * @return Nutrients object
+     */
     public Nutrients getHamperNutrients(){
         return this.hamperNutrients;
     }
+
+    /**
+     * Getter for the daysNeeded
+     * @return Int
+     */
     public int getDaysNeeded(){
         return this.daysNeeded;
     }
