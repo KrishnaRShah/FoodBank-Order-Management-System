8d94789a9a8dfb4f5ae1d4bad99be9bbbeb30605
KrishnaRShah <krishnashah14957@gmail.com>
2022-04-17 13:07:14 -0600

    NutrientsTest(working)

--- FPCode/NutrientsTest.java
+++ FPCode/NutrientsTest.java
@@ -1,12 +1,20 @@
+/**
+ * @author Krishna Shah 30114067<a & German Fonseca 30061209
+ * href="mailto:krishna.shah@ucalgary.ca">krishna.shah@ucalgary.ca</a>
+ * @version 1.8 
+ * @since 0.0
+ */
 package FPCode;
 
 import org.junit.Test;
 import org.junit.Assert;
 import static org.junit.Assert.*;
 
-public class NutrientsTest {
+ public class NutrientsTest {
 
-    //example values for testing purposes
+    /**
+    * These are global variables that will be used in all tests
+    */
     public double grainsPercent=35;
     public double fruitsPercent=30;
     public double meatsPercent=20;
@@ -18,14 +26,18 @@ public class NutrientsTest {
 
     public NutrientsTest(){}
    
-    //test constructor of Nutrients
+    /**
+    * Constructor test with good data
+    */
     @Test
     public void testNutrientsConstructorGoodData() {
         Nutrients first = new Nutrients(grainsPercent,fruitsPercent,meatsPercent,otherPercent,totalCalories);
         assertNotNull("Nutrients constructor did not create an object when given valid inputs", first);
     }
 
-    //test constructor when invalid afguments are provided
+    /**
+    * Constructor test with bad data, that will throw IllegalArgumentExeption
+    */
     @Test
     public void testNutrientsConstructorInvalidData() {
         
@@ -41,6 +53,9 @@ public class NutrientsTest {
         assertEquals("Nutrients constructor did not throw an IllegalArgumentException when given an invalid input", true, correctException);        
     }
 
+    /**
+    * Tests getter functions for Nutrient data
+    */
     @Test 
     public void testNormalGetsNSets(){
         // return percentages
@@ -49,51 +64,61 @@ public class NutrientsTest {
         double ffruit = zeroth.getFruits();
         double fprot = zeroth.getProtein();
         double foth = zeroth.getOther();
-        assertEquals("getGrains did not return the correct value.", fgrain, grainsPercent);
-        assertEquals("getFruits did not return the correct value.", ffruit, fruitsPercent);
-        assertEquals("getProtein did not return the correct value.", fprot, meatsPercent);
-        assertEquals("getOther did not return the correct value.", foth, otherPercent);
+        assertEquals("getGrains did not return the correct value.", fgrain, grainsPercent,0);
+        assertEquals("getFruits did not return the correct value.", ffruit, fruitsPercent,0);
+        assertEquals("getProtein did not return the correct value.", fprot, meatsPercent,0);
+        assertEquals("getOther did not return the correct value.", foth, otherPercent,0);
     }
 
-    //test getter for grains
+    /**
+    * Tests getter for grainsCals
+    */
     @Test
     public void testGetGrainCals() {
         Nutrients third = new Nutrients(grainsPercent,fruitsPercent,meatsPercent,otherPercent,totalCalories);
         double foundGRAINS=third.getGrainCals();
         double expectedGRAINS=grainsPercent*totalCalories/100.0;
-        assertEquals("Method getGrainCals did not return the expected result: ", expectedGRAINS, foundGRAINS);
+        assertEquals("Method getGrainCals did not return the expected result: ", expectedGRAINS, foundGRAINS,0);
     }  
-    //test getter for fruits
+    /**
+    * Tests getter for fruitsCals
+    */
     @Test
     public void testGetFruitCals() {
         Nutrients fourth = new Nutrients(grainsPercent,fruitsPercent,meatsPercent,otherPercent,totalCalories);
         double foundFRUITS=fourth.getFruitCals();
         double expectedFRUITS=fruitsPercent*totalCalories/100.0;
-        assertEquals("Method getFruitCals did not return the expected result: ", expectedFRUITS, foundFRUITS);
+        assertEquals("Method getFruitCals did not return the expected result: ", expectedFRUITS, foundFRUITS,0);
     }  
-    //test getter for meats
+    /**
+    * Tests getter for proteinCals
+    */
     @Test
     public void testGetProteinCals() {
         Nutrients fifth = new Nutrients(grainsPercent,fruitsPercent,meatsPercent,otherPercent,totalCalories);
-        double foundMEATS=fifth.getProtein();
+        double foundMEATS=fifth.getProteinCals();
         double expectedMEATS=meatsPercent*totalCalories/100.0;
-        assertEquals("Method getProteinCals did not return the expected result: ", expectedMEATS, foundMEATS);
+        assertEquals("Method getProteinCals did not return the expected result: ", expectedMEATS, foundMEATS,0);
     }
-    //test getter for other
+    /**
+    * Tests getter for otherCals
+    */
     @Test
     public void testGetOtherCals() {
         Nutrients sixth = new Nutrients(grainsPercent,fruitsPercent,meatsPercent,otherPercent,totalCalories);
         double foundOTHER=sixth.getOtherCals();
         double expectedOTHER=otherPercent*totalCalories/100.0;
-        assertEquals("Method getOtherCals did not return the expected result: ", expectedOTHER, foundOTHER);
+        assertEquals("Method getOtherCals did not return the expected result: ", expectedOTHER, foundOTHER,0);
     }    
     @Test
-    //test getter for total calories
+    /**
+    * Tests getter for total cals
+    */
     public void testGetTotalCals() {
         Nutrients seventh = new Nutrients(grainsPercent,fruitsPercent,meatsPercent,otherPercent,totalCalories);
         double foundCALS=seventh.getTotalCalories();
         double expectedCALS=totalCalories;
-        assertEquals("Method getTotalCalories did not return the expected result: ", expectedCALS, foundCALS);
+        assertEquals("Method getTotalCalories did not return the expected result: ", expectedCALS, foundCALS,0);
     }  
 
 }
