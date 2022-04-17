1f81e4c7c421f35af4ad4c55bff22cd07b6962d1
KrishnaRShah <krishnashah14957@gmail.com>
2022-04-17 13:21:29 -0600

    ClientTest(working)

--- FPCode/ClientTest.java
+++ FPCode/ClientTest.java
@@ -1,17 +1,33 @@
-package FPCode;
+/**
+* @author Krishna Shah 30114067<a
+* href="mailto:krishna.shah@ucalgary.ca">krishna.shah@ucalgary.ca</a>
+* @author Danny Picazo 301271082<a
+* href="mailto:daniel.picazo@ucalgary">daniel.picazo@ucalgary.ca</a>
+* @version 0.8 
+* @since 0.0
+*/
 
+package FPCode;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-
+/**
+ * Class to test Client
+ */
 public class ClientTest {
+
+
+    /**
+    * Tests Client Constructor
+    */
+
     @Test
     public void testClientConstructor(){
         boolean exceptionThrown = false;
 
         try {
             @SuppressWarnings("unused")
-            Client client = new Client(1, "AM");
+            Client client = new Client(1);
         } catch (IllegalArgumentException e) {
             exceptionThrown = true;
         }
@@ -19,11 +35,14 @@ public class ClientTest {
         assertFalse("An exception was thrown when attempting to create a Client.", exceptionThrown);
     }
 
+    /**
+    * Tests Client getters and setters
+    */
     @Test
     public void testClientGetsNSets(){
         boolean exceptionThrown = false;
 
-        Client client = new Client(1, "AM");
+        Client client = new Client(1);
         int id = -1337;
         String type = "you definitely messed something up";
         Nutrients nut = new Nutrients(100, 0, 0, 0, 420);
@@ -45,52 +64,61 @@ public class ClientTest {
                                                                                 (double)2500, 2500);
     }
 
+    /**
+    * Tests all Client types
+    */
+
     @Test
     public void testAllClientTypes(){
         // boolean exceptionThrown = false;
 
-        Client am = new Client(1, "am");
-        Client af = new Client(2, "AF");
-        Client co8 = new Client(3, "CO8");
-        Client cu8 = new Client(4, "Cu8");
+        Client am = new Client(1);
+        Client af = new Client(2);
+        Client co8 = new Client(3);
+        Client cu8 = new Client(4);
 
 
         // assertFalse("An unexpected exception was thrown.", exceptionThrown);
         assertEquals("AM Client type did not return the correct type.", am.getClientType(), "Adult Male");
         assertEquals("AM Client type did not return the correct type.", af.getClientType(), "Adult Female");
-        assertEquals("AM Client type did not return the correct type.", co8.getClientType(), "Child Over 8");
-        assertEquals("AM Client type did not return the correct type.", cu8.getClientType(), "Child Under 8");
+        assertEquals("AM Client type did not return the correct type.", co8.getClientType(), "Child Over 8 years old");
+        assertEquals("AM Client type did not return the correct type.", cu8.getClientType(), "Child Under 8 years old");
 
     }
 
-    @Test
-    public void testInvalidClientType(){
-        boolean exceptionThrown1 = false;
-        boolean exceptionThrown2 = false;
-        boolean exceptionThrown3 = false;
-        @SuppressWarnings("unused")
-        Client client;
+    // @Test
+    // public void testInvalidClientType(){
+    //     boolean exceptionThrown1 = false;
+    //     boolean exceptionThrown2 = false;
+    //     boolean exceptionThrown3 = false;
+    //     @SuppressWarnings("unused")
+    //     Client client;
 
-        try {
-            client = new Client(1, "SWAG");
-        } catch (IllegalArgumentException e) {
-            exceptionThrown1 = true;
-        }
-        try {
-            client = new Client(1, "a m");
-        } catch (IllegalArgumentException e) {
-            exceptionThrown2 = true;
-        }
-        try {
-            client = new Client(1, "");
-        } catch (IllegalArgumentException e) {
-            exceptionThrown3 = true;
-        }
+    //     try {
+    //         client = new Client(-2);
+    //     } catch (IllegalArgumentException e) {
+    //         exceptionThrown1 = true;
+    //     }
+    //     try {
+    //         client = new Client(13);
+    //     } catch (IllegalArgumentException e) {
+    //         exceptionThrown2 = true;
+    //     }
+    //     try {
+    //         client = new Client(22);
+    //     } catch (IllegalArgumentException e) {
+    //         exceptionThrown3 = true;
+    //     }
         
-        assertTrue("An invalid Client type did not throw an IllegalArgumentException.", exceptionThrown1);
-        assertTrue("An invalid Client type did not throw an IllegalArgumentException.", exceptionThrown2);
-        assertTrue("An invalid Client type did not throw an IllegalArgumentException.", exceptionThrown3);
-    }
+    //     assertTrue("An invalid Client type did not throw an IllegalArgumentException.", exceptionThrown1);
+    //     assertTrue("An invalid Client type did not throw an IllegalArgumentException.", exceptionThrown2);
+    //     assertTrue("An invalid Client type did not throw an IllegalArgumentException.", exceptionThrown3);
+    // }
+
+    /**
+    * Tests invalid client id's
+    */
+
 
     @Test
     public void testInvalidClientID(){
@@ -98,8 +126,8 @@ public class ClientTest {
 
         try {
             @SuppressWarnings("unused")
-            Client client1 = new Client(5, "AM");
-            Client client2 = new Client(0, "FM");
+            Client client1 = new Client(5);
+            Client client2 = new Client(0);
         } catch (IllegalArgumentException e) {
             exceptionThrown = true;
         }
@@ -107,19 +135,19 @@ public class ClientTest {
         assertTrue("An invalid Client ID did not throw an IllegalArgumentException.", exceptionThrown);
     }
 
-    @Test
-    public void testIDTypeMismatch(){
-        boolean exceptionThrown = false;
+    // @Test
+    // public void testIDTypeMismatch(){
+    //     boolean exceptionThrown = false;
 
-        try {
-            @SuppressWarnings("unused")
-            Client client1 = new Client(4, "AM");
-            Client client2 = new Client(2, "CU8");
-        } catch (IllegalArgumentException e) {
-            exceptionThrown = true;
-        }
+    //     try {
+    //         @SuppressWarnings("unused")
+    //         Client client1 = new Client(2);
+    //         Client client2 = new Client(2);
+    //     } catch (IllegalArgumentException e) {
+    //         exceptionThrown = true;
+    //     }
 
-        assertTrue("An unmatching ID and type did not throw an IllegalArgumentException.", exceptionThrown);
-    }
+    //     assertTrue("An unmatching ID and type did not throw an IllegalArgumentException.", exceptionThrown);
+    // }
 
 }
